170be3b9196a8d8dca18bd1a057573e06b299b86
Vamshi Krishna <pvamshi@gmail.com>
2016-04-27 20:18:31 -0700

    heapsort ex2

--- heapsort/heapsort-ex.js
+++ heapsort/heapsort-ex.js
@@ -22,3 +22,21 @@ while (h) {
   h = heap1.pop();
 }
 //console.log(hs);
+var heap2 = heapsort.heap();
+
+heap2.add(2);
+heap2.add(23);
+heap2.add(35);
+heap2.add(24);
+heap2.add(5);
+heap2.add(24);
+heap2.add(35);
+heap2.add(4);
+heap2.add(24);
+var h = heap2.pop();
+// for(var i=0; i<3 ; i++){
+while (h) {
+  console.log(h);
+  h = heap2.pop();
+}
+//console.log(hs);
